8fc8305ad85cdfbc49f7adbe3043fb2155f9134a
VanessaLin9 <vanessa7591@gmail.com>
2021-08-25 07:17:24 +0800

    feat: add reataurant index and admin page

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const db = require('./models')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
 
 module.exports = app
