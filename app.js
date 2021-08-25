60557e918aa0a6c2da66b2d1bf97d72d4986ecd2
VanessaLin9 <vanessa7591@gmail.com>
2021-08-26 05:35:13 +0800

    feat: add passport and signin page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./models')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = 3000
 
@@ -11,6 +12,8 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -25,6 +28,6 @@ app.listen(port, () => {
 })
 
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
