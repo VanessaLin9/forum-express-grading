dda3ea1ab41c259004cf3417c537c3eef5dec6c9
VanessaLin9 <vanessa7591@gmail.com>
2021-08-27 04:12:09 +0800

    feat: install multer and add image upload feature

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 
 module.exports = (app, passport) => {
 
@@ -32,14 +34,14 @@ module.exports = (app, passport) => {
 
   // 管理者新增餐廳
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
-  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
 
   //管理者瀏覽一筆餐廳
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
 
   //管理者編輯一家餐廳
   app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
-  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+  app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
 
   //管理者刪除一家餐廳
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
