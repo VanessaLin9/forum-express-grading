d0447ba430424a04053db4629d96ac4194a98151
VanessaLin9 <vanessa7591@gmail.com>
2021-09-03 06:36:29 +0800

    feat: show releated comment on restaurant page and using moment

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 const pageLimit = 10
 
 const restController = {
@@ -51,14 +53,18 @@ const restController = {
     })
   },
   getRestaurant: (req, res) => {
- return Restaurant.findByPk(req.params.id, {
-     include:  Category
-   }).then(restaurant => {
-     return res.render('restaurant', {
-       restaurant: restaurant.toJSON()
-     })
-   })
-}
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    }).then(restaurant => {
+      console.log(restaurant.Comments[0].dataValues)
+      return res.render('restaurant', {
+        restaurant: restaurant.toJSON(),
+      })
+    })
+  }
 }
 
 module.exports = restController
