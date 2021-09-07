d5d0e732c8f7d8e271965b85b212f2f8d146135e
VanessaLin9 <vanessa7591@gmail.com>
2021-09-08 06:47:52 +0800

    refactor: categoryController.getCategories

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -2,6 +2,7 @@ const db = require('../../models')
 const adminService = require('../../services/adminService')
 const Restaurant = db.Restaurant
 const Category = db.Category
+
 const adminController = {
   getRestaurants: (req, res) => {
    adminService.getRestaurants(req, res, (data) => {
